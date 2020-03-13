4f7628aa42f6e9476d413aeb412ca26266119186
Prasanna V <prasannacse2005@gmail.com>
2020-03-13 13:12:46 +0530

    update the log message

--- block_dns.go
+++ block_dns.go
@@ -61,7 +61,7 @@ func NewResponsePrinter(w dns.ResponseWriter) *ResponsePrinter {
 
 // WriteMsg calls the underlying ResponseWriter's WriteMsg method and prints "example" to standard output.
 func (r *ResponsePrinter) WriteMsg(res *dns.Msg) error {
-	fmt.Fprintln(out, "Not blocking the domain")
+	fmt.Fprintln(out, "=========# Welcome to Block DNS #=========")
 	return r.ResponseWriter.WriteMsg(res)
 }
 
